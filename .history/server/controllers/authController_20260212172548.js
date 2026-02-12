540ca159f0cf80d695b6f12e805ed003978aecd7
Authn <authn.sapuarachchi@gmail.com>
2026-02-12 19:03:13 +0530

    auth controller

---
+++ .history/server/controllers/authController_20260212172548.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.registerUser = async (req, res) => {
+    try {
+        const { name, email, password, faculty } = req.body;
+        const userExists = await User.findOne({ email });
+
+        if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+        const user = await User.create({ name, email, password, faculty });
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.status(201).json({ token, user: { id: user._id, name: user.name, role: user.role } });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
