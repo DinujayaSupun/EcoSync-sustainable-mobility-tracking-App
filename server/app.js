6408ad5c846f66ea4acefec77bae3814be9eb313
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-02-19 14:08:11 +0530

    created .env and fixed the app.js

--- server/app.js
+++ server/app.js
@@ -1,32 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const carbonRoutes = require("./routes/carbon.routes");
 const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
+
+// Routes
+app.use("/api/carbon", carbonRoutes);
 
 app.get("/", (req, res) => {
   res.send("Carbon Accounting Engine Backend Running 🚀");
 });
 
+// Error middleware (ALWAYS AFTER ROUTES)
+app.use(errorHandler);
 
-
-
-
+// Database connection
 mongoose
-  .connect("mongodb+srv://admin:VQXl9MkCtgnyxDzg@merncluster.8dgv3ew.mongodb.net/")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
 
 module.exports = app;
-
-//later considerations
