0a5ce2e777a1a2aed52e1c4a64235d03e6109f46
Gayal Thilakarathna <thilakarathnagayal@gmail.com>
2026-04-10 15:14:21 +0530

    Add some features to the CO₂ Saved part

--- server/routes/commuteRoutes.js
+++ server/routes/commuteRoutes.js
@@ -10,6 +10,8 @@ const {
   deleteCommute,
   updateCommute,
   recalculateCo2Saved,
+  getCO2SavingsByTransportMode,
+  getCarUsageImpact,
 } = require("../controllers/commuteController");
 
 // All routes are protected with JWT authentication
@@ -18,6 +20,8 @@ router.get("/history", protect, getCommuteHistory);
 router.get("/emission-summary", protect, getEmissionSummary);
 router.get("/autocomplete", protect, autocompleteLocation);
 router.get("/predict", protect, predictEmission);
+router.get("/co2-savings-by-mode", protect, getCO2SavingsByTransportMode);
+router.get("/car-usage-impact", protect, getCarUsageImpact);
 router.delete("/:id", protect, deleteCommute);
 router.put("/:id", protect, updateCommute);
 router.post("/recalculate-co2", protect, recalculateCo2Saved);
